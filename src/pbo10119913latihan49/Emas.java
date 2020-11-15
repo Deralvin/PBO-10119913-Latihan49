ba0cadd00db8da6262c15ec6b5d23cc04500b7d0
Dilan <ramadhanrizqi21@gmail.com>
2020-11-15 10:00:21 +0700

    Latihan 49

---
+++ src/pbo10119913latihan49/Emas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan49;
+
+/**
+ * @author Muhammad Alvin Rizqi Ramadhan
+ * Kelas : IF10-K
+ * NIM :10119913 
+ */
+public class Emas {
+    private double beratEmas;
+    private double hargaEmas;
+
+    public double getBeratEmas() {
+        return beratEmas;
+    }
+
+    public void setBeratEmas(double beratEmas) {
+        this.beratEmas = beratEmas;
+    }
+
+    public double getHargaEmas() {
+        return hargaEmas;
+    }
+
+    public void setHargaEmas(double  hargaEmas) {
+        this.hargaEmas = hargaEmas;
+    }
+    
+    public double totalBayar(double beratEmas, double hargaEmas){
+        double totalBayar;
+        return totalBayar = beratEmas * hargaEmas;
+    }
+}
---
+++ src/pbo10119913latihan49/Pbo10119913Latihan49.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan49;
+
+import java.util.Scanner;
+
+/**
+/**
+ * @author Muhammad Alvin Rizqi Ramadhan
+ * Kelas : IF10-K
+ * NIM :10119913 
+ */
+public class Pbo10119913Latihan49 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Emas emas = new Emas();
+        System.out.println("");
+        System.out.println("Harga emas adalah = " + emas.totalBayar(15.7, 570000));
+    }
+    
+}
